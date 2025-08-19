ce647428df63a3f193fd17669d83dd1a6a6a3a5f
John Naeder <johnnaeder6537@gmail.com>
2025-08-19 19:27:08 +0700

    Build (dockerfile): update dockerfile and dock-compose for dashboard-api build

--- App/Dashboard/Dashboard.DataAccess/DependencyInjection.cs
+++ App/Dashboard/Dashboard.DataAccess/DependencyInjection.cs
@@ -15,25 +15,16 @@ public static class DependencyInjection
         var Context = builder.Configuration.GetConnectionString("WebbanhangDB");
 
         Guard.Against.Null(Context, message: "Connection string 'WebbanhangDB' not found.");
-        
-        // Register TimeProvider
-        builder.Services.AddSingleton(TimeProvider.System);
-        
-        // Register interceptors
         builder.Services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
         builder.Services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
 
-        builder.Services.AddDbContext<WebbanhangDbContext>((serviceProvider, options) =>
+        builder.Services.AddDbContext<WebbanhangDbContext>((sp, options)=>
         {
-            options.UseSqlServer(Context, sqlOptions =>
-            {
-                sqlOptions.MigrationsAssembly(typeof(WebbanhangDbContext).Assembly.FullName);
-            });
-            
-            // Get interceptors from service provider
-            //var interceptors = serviceProvider.GetServices<ISaveChangesInterceptor>();
-            //options.AddInterceptors(interceptors);
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            options.UseSqlServer(Context);
         });
+
+        builder.Services.AddSingleton(TimeProvider.System);
 
         builder.Services.AddScoped<WebbanhangDbContextInitializer>();
     }
